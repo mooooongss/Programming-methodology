0cff3394525f0d4a70335b4d4edc861c40e79c05
MediaLab <MediaLab@DESKTOP-0DG70KA>
2023-09-15 10:17:05 +0900

    9/15

---
+++ Hello/src/LiteralTest.java
@@ -0,0 +1,18 @@
+
+public class LiteralTest {
+
+	public static void main(String[] args) {
+		int a = 100;
+		int b = 0b1100100;
+		int c = 0144;
+		int d = 0x64;
+		System.out.println("10진수 100 = " + a);
+		System.out.println("2진수 100 = " + b);
+		System.out.println("8진수 100 = " + c);
+		System.out.println("16진수 100 = " + d);
+		System.out.println("2진수 100 = " + 0b111);
+		System.out.println("8진수 100 = " + 0777);
+		System.out.println("16진수 100 = " + 0xfff);
+	}
+
+}

fe534c87809c1c12306e87f85537175f13068cce
MSB <MSB@MOOOOONG>
2023-10-01 05:23:19 +0900

    10/1 강의

--- Hello/src/EnumEx.java
+++ Hello/src/EnumEx.java
@@ -19,6 +19,28 @@ public class EnumEx {
 		System.out.println(weekTest2);
 		
 		System.out.println();
+		
+		enum WeekEng {
+			월("Mon"),
+			화("Tue"),
+			수("Wed"),
+			목("Thu"),
+			금("Fri"),
+			토("Sat"),
+			일("Sun");
+			
+			final private String engName;
+			private WeekEng(String name) {
+				this.engName = name;
+			}
+			
+			public String getName() {
+				return engName;
+			}
+		};
+		
+		for(WeekEng day : WeekEng.values()) {
+			System.out.println(day.getName());
+		}
 	}
-
 }

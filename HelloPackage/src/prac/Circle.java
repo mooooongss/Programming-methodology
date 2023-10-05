9ff0b35f46e9ab993d2e2fe4624c326e24d90a91
MediaLab <MediaLab@DESKTOP-0DG70KA>
2023-10-05 14:47:37 +0900

    10/5 수업

---
+++ HelloPackage/src/prac/Circle.java
@@ -0,0 +1,24 @@
+package prac;
+
+public class Circle {
+	private final double PI = 3.14;
+	
+	public int radius;
+	public String name;
+	
+	public Circle() {
+		
+	}
+	
+	public double getPI() {
+		return this.PI;
+	}
+	
+	public double getArea() {
+		return this.PI * radius * radius;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Class and Object Practice");
+	}
+}

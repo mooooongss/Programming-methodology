ef59a683df3ac7a8466b41864100cda147f53475
MediaLab <MediaLab@DESKTOP-0DG70KA>
2023-10-13 10:30:30 +0900

    Week6

---
+++ HelloPackage/src/ass/PhoneBook.java
@@ -0,0 +1,29 @@
+package ass;
+
+import java.util.Scanner;
+
+/*
+class Phone {
+	for(int i = 0; i < a; i++) {
+		Scanner scanner1 = new Scanner(System.in);
+		
+		System.out.print("Name and Phonenumber (ex. 홍길동 010-1234-5678) >> ");
+		String phonenum = scanner1.next();
+	}
+}
+*/
+
+public class PhoneBook {
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Number of Person >> ");
+		int num = scanner.nextInt();
+		
+		// Phone(num);
+	}
+
+}
